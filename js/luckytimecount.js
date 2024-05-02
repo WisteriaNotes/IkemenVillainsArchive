0ccf02d7746d7015fa237ab86b99e910b29923bd
WisteriaNotes <99854997+WisteriaNotes@users.noreply.github.com>
2024-05-01 22:48:00 -0700

    updated with new en event

--- js/luckytimecount.js
+++ js/luckytimecount.js
@@ -14,13 +14,16 @@
 	];
 
 	const timeStr_en = [
-	"2024-04-16T20:00-07:00","2024-04-17T04:00-07:00",
-	"2024-04-18T21:00-07:00","2024-04-19T05:00-07:00",
-	"2024-04-20T08:00-07:00","2024-04-21T08:00-07:00",
-	"2024-04-22T21:00-07:00","2024-04-23T05:00-07:00",
-	"2024-04-24T19:00-07:00","2024-04-25T01:00-07:00",
-	"2024-04-27T19:00-07:00","2024-04-28T01:00-07:00",
-	"2024-04-28T18:00-07:00","2024-04-29T18:00-07:00"];
+	"2024-05-01T18:00-07:00","2024-05-02T00:00-07:00",
+	"2024-05-01T18:00-07:00","2024-05-02T00:00-07:00",
+	"2024-05-02T12:00-07:00","2024-05-03T12:00-07:00",
+	"2024-05-03T14:00-07:00","2024-05-04T02:00-07:00",
+	"2024-05-04T06:00-07:00","2024-05-05T06:00-07:00",
+	"2024-05-05T16:00-07:00","2024-05-06T00:00-07:00",
+	"2024-05-07T00:00-07:00","2024-05-08T00:00-07:00",
+	"2024-05-09T18:00-07:00","2024-05-10T02:00-07:00",
+	"2024-05-11T00:00-07:00","2024-05-12T00:00-07:00",
+	"2024-05-14T18:00-07:00","2024-05-15T17:59-07:00"];
 
 	const times_jp = [];
 	const times_en = [];
@@ -57,7 +60,10 @@
 	"time-4-en",
 	"time-5-en",
 	"time-6-en",
-	"time-7-en"
+	"time-7-en",
+	"time-8-en",
+	"time-9-en",
+	"time-10-en"
 	];
 
 	for (let i = 0; i < times_jp.length; i += 2) {
@@ -79,8 +85,28 @@
 			document.getElementById(ids_jp[count_jp]).innerHTML = "<b>Escort x2 - </b> " + times_jp[i] + " ~ " + times_jp[i + 1];
 			count_jp++;
 		}
-		
 	}
+	for (let i = 0; i < times_en.length; i += 2) {
+		if (!times_en[i + 1]) {
+			break;
+		}
+		if(count_en == 0 || count_en == 2 || count_en == 4 || count_en == 6)
+		{
+			document.getElementById(ids_en[count_en]).innerHTML = "<b>Story x2 - </b> " + times_en[i] + " ~ " + times_en[i + 1];
+			count_en++;
+		}
+		else if(count_en == 8)
+		{
+			document.getElementById(ids_en[count_en]).innerHTML = "<b>Story x3 - </b> " + times_en[i] + " ~ " + times_en[i + 1];
+			count_en++;
+		}
+		else if(count_en == 1 || count_en == 3 || count_en == 5 || count_en == 7 || count_en == 9)
+		{
+			document.getElementById(ids_en[count_en]).innerHTML = "<b>Escort x2 - </b> " + times_en[i] + " ~ " + times_en[i + 1];
+			count_en++;
+		}
+	}
+
 	/*for (let i = 0; i < times_jp.length; i += 2) {
 		if (!times_jp[i + 1]) {
 			break;
@@ -91,7 +117,7 @@
 		}
 	}*/
 
-	for (let i = 0; i < times_en.length; i += 2) {
+	/*for (let i = 0; i < times_en.length; i += 2) {
 		if (!times_en[i + 1]) {
 			break;
 		}
@@ -99,4 +125,4 @@
 			document.getElementById(ids_en[count_en]).innerHTML = times_en[i] + " ~ " + times_en[i + 1];
 			count_en++;
 		}
-	}
+	}*/
