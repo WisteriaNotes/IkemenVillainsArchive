ae3b39d7e59429a65a546d10f5f169ec8de39e4d
WisteriaNotes <99854997+WisteriaNotes@users.noreply.github.com>
2024-05-09 01:04:36 -0700

    updated with new jp gacha

--- js/luckytimecount.js
+++ js/luckytimecount.js
@@ -65,6 +65,82 @@
 	"time-10-en"
 	];
 
+	var jp_type = "s";
+	var en_type = "c";
+
+	switch(jp_type){
+		case "s":
+			for (let i = 0; i < times_jp.length; i += 2) {
+				if (!times_jp[i + 1]) {
+					break;
+				}
+				else{
+					document.getElementById(ids_jp[count_jp]).innerHTML = times_jp[i] + " ~ " + times_jp[i + 1];
+					count_jp++;
+				}
+			}
+			break;
+		case "c":
+			for (let i = 0; i < times_jp.length; i += 2) {
+				if (!times_jp[i + 1]) {
+					break;
+				}
+				if(count_jp == 1 || count_jp == 4 || count_jp == 6 || count_jp == 9)
+				{
+					document.getElementById(ids_jp[count_jp]).innerHTML = "<b>Story x2 - </b> " + times_jp[i] + " ~ " + times_jp[i + 1];
+					count_jp++;
+				}
+				else if(count_jp == 1 || count_jp == 5 || count_jp == 12)
+				{
+					document.getElementById(ids_jp[count_jp]).innerHTML = "<b>Escort x3 - </b> " + times_jp[i] + " ~ " + times_jp[i + 1];
+					count_jp++;
+				}
+				else if(count_jp == 0 || count_jp == 2 || count_jp == 3 || count_jp == 5 || count_jp == 7 || count_jp == 8 || count_jp == 10)
+				{
+					document.getElementById(ids_jp[count_jp]).innerHTML = "<b>Escort x2 - </b> " + times_jp[i] + " ~ " + times_jp[i + 1];
+					count_jp++;
+				}
+			}
+			break;
+	}
+
+	switch(en_type){
+		case "s":
+			for (let i = 0; i < times_en.length; i += 2) {
+				if (!times_en[i + 1]) {
+					break;
+				}
+				else{
+					document.getElementById(ids_en[count_en]).innerHTML = times_en[i] + " ~ " + times_en[i + 1];
+					count_en++;
+				}
+			}
+			break;
+			
+		case "c":
+			for (let i = 0; i < times_en.length; i += 2) {
+				if (!times_en[i + 1]) {
+					break;
+				}
+				if(count_en == 0 || count_en == 2 || count_en == 4 || count_en == 6)
+				{
+					document.getElementById(ids_en[count_en]).innerHTML = "<b>Story x2 - </b> " + times_en[i] + " ~ " + times_en[i + 1];
+					count_en++;
+				}
+				else if(count_en == 8)
+				{
+					document.getElementById(ids_en[count_en]).innerHTML = "<b>Story x3 - </b> " + times_en[i] + " ~ " + times_en[i + 1];
+					count_en++;
+				}
+				else if(count_en == 1 || count_en == 3 || count_en == 5 || count_en == 7 || count_en == 9)
+				{
+					document.getElementById(ids_en[count_en]).innerHTML = "<b>Escort x2 - </b> " + times_en[i] + " ~ " + times_en[i + 1];
+					count_en++;
+				}
+			}
+			break;
+	}
+
 	/*for (let i = 0; i < times_jp.length; i += 2) {
 		if (!times_jp[i + 1]) {
 			break;
@@ -85,7 +161,7 @@
 			count_jp++;
 		}
 	}*/
-	for (let i = 0; i < times_en.length; i += 2) {
+	/*for (let i = 0; i < times_en.length; i += 2) {
 		if (!times_en[i + 1]) {
 			break;
 		}
@@ -104,9 +180,9 @@
 			document.getElementById(ids_en[count_en]).innerHTML = "<b>Escort x2 - </b> " + times_en[i] + " ~ " + times_en[i + 1];
 			count_en++;
 		}
-	}
+	}*/
 
-	for (let i = 0; i < times_jp.length; i += 2) {
+	/*for (let i = 0; i < times_jp.length; i += 2) {
 		if (!times_jp[i + 1]) {
 			break;
 		}
@@ -114,7 +190,7 @@
 			document.getElementById(ids_jp[count_jp]).innerHTML = times_jp[i] + " ~ " + times_jp[i + 1];
 			count_jp++;
 		}
-	}
+	}*/
 
 	/*for (let i = 0; i < times_en.length; i += 2) {
 		if (!times_en[i + 1]) {
